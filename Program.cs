c6fb79d053cca5f89b4c92185aa75b90a178a1ad
Nulenka <117688741+Nulenka@users.noreply.github.com>
2023-01-11 14:33:28 +0300

    Update Program.cs
    Whole code added

--- Program.cs
+++ Program.cs
@@ -1,14 +1,38 @@
-﻿Console.WriteLine("Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.");
+Console.WriteLine("Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.");
 
-string[] array = BaseArray();
-string[] result = Less3Array(array, 3);
+string[] FirstArray = BaseArray();
+string[] result = new string[FirstArray.Length];
 
-Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+Console.Write($"{string.Join(" ", FirstArray)} -> ");
 
+Less3Array(FirstArray, result);
+WriteArray(result);
 
+void Less3Array(string[] FirstArray, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if(FirstArray[i].Length <= 3)
+        {
+            result[count] = FirstArray[i];
+            count++;
+        }
+    }
+}
+
+void WriteArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
 string[] BaseArray()
 {
     Console.Write("Введите значения массива через пробел: ");
     return Console.ReadLine().Split(" ");
 }
+Console.WriteLine("Хорошего дня!");
